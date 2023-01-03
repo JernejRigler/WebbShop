7ed57e4b11eb639d6fd481f6c2211559a6ceb512
Jernej Rigler <jernejrigler@gmail.com>
2023-01-03 16:26:22 +0100

    frontend styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
-        <a href="\">WebbShop</a>
+        <a id="Naslov" href="\">
+          WebbShop
+        </a>
       </header>
-      <main>List item</main>
+      <main>
+        <h1>Priporočeni izdelki</h1>
+        <div className="seznamIzdelkov">
+          {data.izdelki.map((izdelek) => (
+            <div className="izdelek" key={izdelek.alt}>
+              <a href={`/product/${izdelek.alt}`}>
+                <img src={izdelek.slika} alt={izdelek.imeIzdelka} />
+              </a>
+              <div className="imeCena">
+                <a href={`/product/${izdelek.alt}`}>
+                  <p>{izdelek.imeIzdelka}</p>
+                </a>
+                <p>{izdelek.cena}</p>
+                <button>V košarico</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
