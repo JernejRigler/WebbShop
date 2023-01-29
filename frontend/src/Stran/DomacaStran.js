18fe42c0275f88de08d717d2a0bbdd8e7c0a52fa
Jernej Rigler <jernejrigler@gmail.com>
2023-01-29 09:30:06 +0100

    dodal reducer hook za fetch

--- frontend/src/Stran/DomacaStran.js
+++ frontend/src/Stran/DomacaStran.js
@@ -1,14 +1,40 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import logger from 'use-reducer-logger';
 //import data from '../data';
 
+const reducer = (stanje, akcija) => {
+  switch (akcija.tip) {
+    case 'FETCH_REQUEST':
+      return { ...stanje, nalaganje: 1 };
+    case 'FETCH_SUCCESS':
+      return { ...stanje, izdelki: akcija.payload, nalaganje: 0 };
+    case 'FETCH_FAIL':
+      return { ...stanje, nalaganje: 0, error: akcija.payload };
+    default:
+      return stanje;
+  }
+};
+
 function DomacaStran() {
-  const [izdelki, nastaviIzdelke] = useState([]);
+  const [{ nalaganje, error, izdelki }, nalozi] = useReducer(logger(reducer), {
+    izdelki: [],
+    nalaganje: 1,
+    error: '',
+  });
+  //const [izdelki, nastaviIzdelke] = useState([]);
   useEffect(() => {
     const dobiPodatke = async () => {
-      const odgovor = await axios.get('/api/izdelki');
-      nastaviIzdelke(odgovor.data);
+      nalozi({ tip: 'FETCH_REQUEST' });
+      try {
+        const odgovor = await axios.get('/api/izdelki');
+        nalozi({ tip: 'FETCH_SUCCESS', payload: odgovor.data });
+      } catch (err) {
+        nalozi({ tip: 'FETCH_FAIL', payload: err.message });
+      }
+
+      //nastaviIzdelke(odgovor.data);
     };
     dobiPodatke();
   }, []);
@@ -16,20 +42,26 @@ function DomacaStran() {
     <div>
       <h1>Priporočeni izdelki</h1>
       <div className="seznamIzdelkov">
-        {izdelki.map((izdelek) => (
-          <div className="izdelek" key={izdelek.alt}>
-            <Link to={`/izdelek/${izdelek.alt}`}>
-              <img src={izdelek.slika} alt={izdelek.imeIzdelka} />
-            </Link>
-            <div className="imeCena">
+        {nalaganje ? (
+          <div>Nalaganje...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          izdelki.map((izdelek) => (
+            <div className="izdelek" key={izdelek.alt}>
               <Link to={`/izdelek/${izdelek.alt}`}>
-                <p>{izdelek.imeIzdelka}</p>
+                <img src={izdelek.slika} alt={izdelek.imeIzdelka} />
               </Link>
-              <p>{izdelek.cena}</p>
-              <button>V košarico</button>
+              <div className="imeCena">
+                <Link to={`/izdelek/${izdelek.alt}`}>
+                  <p>{izdelek.imeIzdelka}</p>
+                </Link>
+                <p>{izdelek.cena}</p>
+                <button>V košarico</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
